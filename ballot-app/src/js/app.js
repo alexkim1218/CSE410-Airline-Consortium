b6e13a23ad42350b337b81abee7d562e5ba8944b
choongliang <choongli@buffalo.edu>
2019-05-07 11:48:14 -0400

    DOne
    local storage now stores airline balance and seats

--- ballot-app/src/js/app.js
+++ ballot-app/src/js/app.js
@@ -3,6 +3,7 @@ App = {
   contracts: {},
   names: new Array(),
   seats: new Array(),
+  balances: new Array(),
   url: 'http://127.0.0.1:7545',
   // chairPerson:null,
   // currentAccount:null,
@@ -12,20 +13,65 @@ App = {
       var proposalsRow = $('#proposalsRow');
       var proposalTemplate = $('#proposalTemplate');
 
-      for (i = 0; i < data.length; i ++) {
-        proposalTemplate.find('.panel-title').text(data[i].name);
-        proposalTemplate.find('img').attr('src', data[i].picture);
-        proposalTemplate.find('.btn-vote').attr('data-id', data[i].id);
-        proposalTemplate.find('.seat-panel').text(data[i].seat).addClass('seat'+i);
-        proposalsRow.append(proposalTemplate.html());
+      // test if local storage exists
+      var storage_data = localStorage.getItem(data[0].name);
+      console.log(storage_data);
 
-        // Remove current class from the template after appending to html
-        proposalTemplate.find('.seat-panel').text(data[i].seat).removeClass('seat'+i);
-        App.names.push(data[i].name);
-        App.seats.push(data[i].seat);
+      currentAirline = localStorage.getItem('current');
+      console.log('see current', currentAirline);
+
+      // if it does
+      if (storage_data !== null) {
+        for(i = 0; i < data.length; i++) {
+          storage_data = localStorage.getItem(data[i].name);
+          console.log(storage_data);
+          proposalTemplate.find('.panel-title').text(data[i].name);
+          proposalTemplate.find('img').attr('src', data[i].picture);
+          proposalTemplate.find('.btn-vote').attr('data-id', data[i].id);
+          proposalTemplate.find('.seat-panel').text(storage_data[0]).addClass('seat'+i);
+          proposalTemplate.find('.airline-address').addClass('airline'+i);
+          proposalTemplate.find('.panel-balance').text(storage_data.substring(2,5)).addClass('bal_'+i);
+          proposalsRow.append(proposalTemplate.html());
+
+          // Remove current class from the template after appending to html
+          proposalTemplate.find('.seat-panel').text(data[i].seat).removeClass('seat'+i);
+          proposalTemplate.find('.airline-address').removeClass('airline'+i);
+          proposalTemplate.find('.panel-balance').removeClass('bal_'+i);
+          App.names.push(data[i].name);
+          App.seats.push(parseInt(storage_data[0]));
+          App.balances.push(parseInt(storage_data.substring(2,5)));
+        }
       }
-    }); 
-    return App.initWeb3();
+      else {
+        for (i = 0; i < data.length; i ++) {
+          proposalTemplate.find('.panel-title').text(data[i].name);
+          proposalTemplate.find('img').attr('src', data[i].picture);
+          proposalTemplate.find('.btn-vote').attr('data-id', data[i].id);
+          proposalTemplate.find('.seat-panel').text(data[i].seat).addClass('seat'+i);
+          proposalTemplate.find('.airline-address').addClass('airline'+i);
+          proposalTemplate.find('.panel-balance').text(data[i].balance).addClass('bal_'+i);
+          proposalsRow.append(proposalTemplate.html());
+
+          // Remove current class from the template after appending to html
+          proposalTemplate.find('.seat-panel').text(data[i].seat).removeClass('seat'+i);
+          proposalTemplate.find('.airline-address').removeClass('airline'+i);
+          proposalTemplate.find('.panel-balance').removeClass('bal_'+i);
+          App.names.push(data[i].name);
+          App.seats.push(data[i].seat);
+          App.balances.push(data[i].balance);
+
+          // Store to local storage
+          // Looks like 'ANA': [seat, bal]
+          //   i.e.  -> 'ANA': [3, 0]
+          var temp_list = new Array();
+          temp_list.push(data[i].seat);
+          temp_list.push(data[i].balance);
+          localStorage.setItem(data[i].name,temp_list);
+          console.log('initialization done');
+        }
+      }
+      });
+        return App.initWeb3();
   },
 
   initWeb3: function() {
@@ -40,7 +86,7 @@ App = {
 
     ethereum.enable();
 
-    //App.populateAddress();
+    App.populateAddress();
     return App.initContract();
   },
 
@@ -64,18 +110,20 @@ App = {
     // $(document).on('click', '#register', function(){ App.handleRegister(chairperson); });
   },
 
-  // populateAddress : function(){
-  //   new Web3(new Web3.providers.HttpProvider(App.url)).eth.getAccounts((err, accounts) => {
-  //     jQuery.each(accounts,function(i){
-  //       if(web3.eth.coinbase != accounts[i]){
-  //         var optionElement = '<option value="'+accounts[i]+'">'+accounts[i]+'</option';
-  //         jQuery('#enter_address').append(optionElement);  
-  //       }else {
-  //         jQuery('#chairperson').append(accounts[i]);
-  //       }
-  //     });
-  //   });
-  // },
+  populateAddress : function(){
+    new Web3(new Web3.providers.HttpProvider(App.url)).eth.getAccounts((err, accounts) => {
+      jQuery.each(accounts,function(i){
+        if(web3.eth.coinbase != accounts[i]){
+          var optionElement = '<option value="'+accounts[i]+'">'+accounts[i]+'</option';
+          var temp_str = accounts[i].substring(0,16);
+          temp_str += '...';
+          jQuery('.airline'+(i-1)).append(temp_str); 
+        }else {
+          jQuery('#chairperson').append(accounts[i]);
+        }
+      });
+    });
+  },
 
   // getChairperson : function(){
   //   App.contracts.response.deployed().then(function(instance) {
@@ -122,7 +170,7 @@ App = {
       alert(App.names[proposalId-1] + ' is fully booked');
     } else if (typeof currentAirline !== 'undefined' && currentAirline == proposalId-1) {
       alert('You already have this ticket');
-    } else if (typeof currentAirline !== 'undefined') {
+    } else if (typeof currentAirline !== 'undefined' && currentAirline !== null) {
       console.log('currently', currentAirline);
       web3.eth.getAccounts(function(error, accounts) {
         var account = accounts[0];
@@ -140,10 +188,22 @@ App = {
                     App.seats[proposalId-1] -= 1;
                     console.log('seat remain', App.seats[proposalId-1]);
 
-                    // Old airline add seat
+                    // Old airline add seat (altho its called current airline)
                     App.seats[currentAirline] += 1;
                     var seat_add_back = '.seat' + currentAirline;
                     jQuery(seat_add_back).empty().append(App.seats[currentAirline]);
+
+                    // Old airline update balance
+                    App.balances[currentAirline] -= 100;
+                    console.log('current balance', App.balances[currentAirline]);
+                    var balance_to_update = '.bal_' + currentAirline;
+                    console.log('bal class',balance_to_update);
+                    jQuery(balance_to_update).empty().append(App.balances[currentAirline]);
+
+                    var temp_list = new Array();
+                    temp_list.push(App.seats[currentAirline]);
+                    temp_list.push(App.balances[currentAirline]);
+                    localStorage.setItem(App.names[currentAirline],temp_list);
 
                     // New airline reduce seat
                     var old_seat = '.seat' + (proposalId-1);
@@ -151,6 +211,17 @@ App = {
                     console.log('id to change', old_seat);
                     currentAirline = proposalId-1;
                     console.log('current airline', currentAirline);
+
+                    // Update balance then store to storage
+                    App.balances[proposalId-1] += 100;
+                    balance_to_update = '.bal_' + (proposalId-1);
+                    jQuery(balance_to_update).empty().append(App.balances[proposalId-1]);
+
+                    temp_list = new Array();
+                    temp_list.push(App.seats[proposalId-1]);
+                    temp_list.push(App.balances[proposalId-1]);
+                    localStorage.setItem(App.names[proposalId-1],temp_list);
+                
                   }
                   else {
                     alert(account + " changing not done successfully due to revert");
@@ -182,6 +253,20 @@ App = {
                     console.log(seat_to_change);
                     currentAirline = proposalId-1;
                     console.log('current airline', currentAirline);
+                    var temp_airline = new Array();
+                    temp_airline.push(currentAirline);
+                    console.log('temp',temp_airline);
+                    localStorage.setItem('current', temp_airline);
+
+                    // Update balance then store to storage
+                    App.balances[proposalId-1] += 100;
+                    var balance_to_update = '.bal_' + (proposalId-1);
+                    jQuery(balance_to_update).empty().append(App.balances[proposalId-1]);
+
+                    var temp_list = new Array();
+                    temp_list.push(App.seats[proposalId-1]);
+                    temp_list.push(App.balances[proposalId-1]);
+                    localStorage.setItem(App.names[proposalId-1], temp_list);        
                   }
                   else {
                     alert(account + " changing not done successfully due to revert");
